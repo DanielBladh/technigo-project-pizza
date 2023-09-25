c187ff90e66ed429e326380925c5fe7dac43e1fa
DanielBladh <DanielBladh92@outlook.com>
2023-09-25 13:21:07 +0200

    Step-2 Complete

--- code/script.js
+++ code/script.js
@@ -7,9 +7,22 @@ alert(
 )
 let yourName = prompt("Please enter your name below:");
   alert(`Nice to meet you, ${yourName}`);
-  
-// Step 2 - Food choice
 
+// Step 2 - Food choice
+let foodChoice = prompt(`What type of food would you like to order? \nEnter a number:\n1. Pizza\n2. Pasta\n3. Salad`)
+switch(foodChoice) {
+  case "1":
+    alert("You've chosen Pizza!")
+    break;
+  
+    case "2":
+      alert("You've chosen Pasta!")
+      break;
+
+  case "3":
+    alert("You've chosen Salad!")
+    break;
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
